129a45f16567d6c9ae10cff0e1057a56ca271054
enocgc <enoc.gc92@gmail.com>
2018-01-05 11:38:37 -0600

    click season menu
    cesar y enoc <3♥

---
+++ js/desktop-menu-links.js
@@ -0,0 +1,7 @@
+function load_userlist() {
+ document.getElementById("contenedor-1").innerHTML='<object type="text/html" style="width: 100%; height: calc(100vh - 120px);" data="listuser.html" ></object>';
+}
+
+function load_listseason() {
+ document.getElementById("contenedor-1").innerHTML='<object type="text/html" style="width: 100%; height: calc(100vh - 120px);" data="listseason.html" ></object>';
+}
