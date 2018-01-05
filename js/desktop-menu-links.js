9e519ba59823baf77183685fa1b0401eaa3c8fba
csarotarola <35034740+csarotarola@users.noreply.github.com>
2018-01-05 11:50:57 -0600

    Acomodo del desktop

--- js/desktop-menu-links.js
+++ js/desktop-menu-links.js
@@ -1,7 +1,7 @@
 function load_userslist() {
- document.getElementById("contenedor-1").innerHTML='<object type="text/html" style="width: 100%; height: calc(100vh - 120px);" data="listusers.html" ></object>';
+ document.getElementById("contenedor-1").innerHTML='<object type="text/html" style="width: 100%; height: calc(100vh - 86px);" data="listusers.html" ></object>';
 }
 
 function load_listseason() {
- document.getElementById("contenedor-1").innerHTML='<object type="text/html" style="width: 100%; height: calc(100vh - 120px);" data="listseason.html" ></object>';
+ document.getElementById("contenedor-1").innerHTML='<object type="text/html" style="width: 100%; height: calc(100vh - 86px);" data="listseason.html" ></object>';
 }
