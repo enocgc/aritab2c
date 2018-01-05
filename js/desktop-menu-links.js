7dca866a95045ca0de55c58d36bae40ce5abbb10
enocgc <enoc.gc92@gmail.com>
2018-01-05 12:10:26 -0600

    History

--- js/desktop-menu-links.js
+++ js/desktop-menu-links.js
@@ -6,5 +6,8 @@ function load_listseason() {
  document.getElementById("contenedor-1").innerHTML='<object type="text/html" style="width: 100%; height: calc(100vh - 86px);" data="listseason.html" ></object>';
 }
 function load_adduser() {
- document.getElementById("contenedor-1").innerHTML='<object type="text/html" style="width: 100%; height: calc(100vh - 120px);" data="adduser.html" ></object>';
+ document.getElementById("contenedor-1").innerHTML='<object type="text/html" style="width: 100%; height: calc(100vh - 86px);" data="adduser.html" ></object>';
+}
+function cancel() {
+  window.history.back();
 }
