1cd0a99c025bfe3b5ded73dbccec15a6f121263c
enocgc <enoc.gc92@gmail.com>
2018-01-08 20:58:06 -0600

    Delete app.js

--- js/app.js
+++
@@ -1,8 +0,0 @@
-var appRouter = angular.module("aritab2c",[]);
-var appRouterArita = angular.module("aritab2c",['ngRoute']);
-
-appRouterArita.filter('paginacion', function(){
-   return function(data, start){
-     return data.slice(start);
-   }
-});
