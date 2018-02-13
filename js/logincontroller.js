4fe3c292249400068860587c87dbb555a86a9b7e
andrey2503 <andrey_tv_@hotmail.com>
2018-02-12 21:25:55 -0600

    login update

--- js/logincontroller.js
+++ js/logincontroller.js
@@ -2,7 +2,7 @@ var loginApp = angular.module("loginApp", []);
 loginApp.controller("loginController", function($scope, $http) {
 
     $scope.login = function() {
-     console.log("entro login");
+     console.log("entro login del scope");
         $http.post('php/login.php', {'usuario': $scope.usuario,'password': $scope.contrasena})
         .success(function(response) {
             console.log(response);
@@ -16,27 +16,11 @@ loginApp.controller("loginController", function($scope, $http) {
             alert('error');
         });
     }
-
-    function login(){
-      console.log("entro login");
-         $http.post('php/login.php', {'usuario': $scope.usuario,'password': $scope.contrasena})
-         .success(function(response) {
-             console.log(response);
-             if(response==1){
-                 window.location.replace("views/desktop.html");
-             }else{
-                 alert("Usuario o contrasena incorrecto");
-
-             }
-         }, function errorCallback(response) {
-             alert('error');
-         });
-    }
     
     $scope.checkIfEnterKeyWasPressed = function($event){
         var keyCode = $event.which || $event.keyCode;
         if (keyCode === 13) {
-    login();
+            $scope.login();
           console.log("login");
         }
 
