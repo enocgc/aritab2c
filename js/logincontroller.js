aeb1ba9eebee571208e92847d2f90aecf417c0c7
enocgc <enoc.gc92@gmail.com>
2018-02-12 21:05:46 -0600

    logn whit key presed

--- js/logincontroller.js
+++ js/logincontroller.js
@@ -1,7 +1,8 @@
 var loginApp = angular.module("loginApp", []);
 loginApp.controller("loginController", function($scope, $http) {
+
     $scope.login = function() {
-console.log("entro login");
+     console.log("entro login");
         $http.post('php/login.php', {'usuario': $scope.usuario,'password': $scope.contrasena})
         .success(function(response) {
             console.log(response);
@@ -15,4 +16,27 @@ console.log("entro login");
             alert('error');
         });
     }
+    function login(){
+      console.log("entro login");
+         $http.post('php/login.php', {'usuario': $scope.usuario,'password': $scope.contrasena})
+         .success(function(response) {
+             console.log(response);
+             if(response==1){
+                 window.location.replace("views/desktop.html");
+             }else{
+                 alert("Usuario o contrasena incorrecto");
+
+             }
+         }, function errorCallback(response) {
+             alert('error');
+         });
+    }
+    $scope.checkIfEnterKeyWasPressed = function($event){
+        var keyCode = $event.which || $event.keyCode;
+        if (keyCode === 13) {
+    login();
+          console.log("login");
+        }
+
+      }
 });
